32e0a48e4c393f103d16c313f67f55bc5b8bc5b5
Ishan-3213 <ishan18112000@gmail.com>
2025-05-29 14:22:08 -0400

    background image pushed

--- nestle-chatbot-frontend/src/App.tsx
+++ nestle-chatbot-frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div
   className="app"
   style={{
-    backgroundImage: "url('/NestleBG.jpg')",
+    backgroundImage: "url('../public/NestleBG.jpg')",
     backgroundSize: "cover",
     backgroundPosition: "left center",
     height: "100vh",
